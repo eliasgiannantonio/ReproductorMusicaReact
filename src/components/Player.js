2267ed47e90eb0d9713070af27fd99369af5098c
eliasgiannantonio <giannantonioelias@gmail.com>
2024-07-08 17:52:55 -0300

    Setup inicial

---
+++ src/components/Player.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Player = () => {
+    return(
+        <div className="player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type="range" name="" id="" />
+                <p>End Time</p>
+            </div>
+            <div className="play-control">
+                
+            </div>
+        </div>
+        
+    )
+}
+
+export default Player;
