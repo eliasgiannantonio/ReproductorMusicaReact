e268f026786e9d2402d14593c298445aa5778a0a
eliasgiannantonio <giannantonioelias@gmail.com>
2024-07-11 16:17:47 -0300

    Implementación del toggle de librería

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //Agrego componentes
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 //Agrego estilos
 import "./styles/app.scss"
 //Agrego music data
@@ -10,19 +11,49 @@ import data from "./util";
 
 
 function App() {
+  //Ref
+  const audioRef = useRef(null);
   //State
   const [songs, setSongs] = useState(data())
   const[currentSong, setCurrentSong] = useState(songs[0]);
   const[isPlaying, setIsPlaying] = useState(false)
+  const[songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({...songInfo, currentTime: current, duration})
+  };
+  
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong}/>
       <Player 
+        audioRef={audioRef}
         setIsPlaying={setIsPlaying}
         isPlaying={isPlaying} 
         currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
       />
-      <Library songs={songs} setCurrentSong={setCurrentSong}/>
+      <Library 
+        audioRef={audioRef}
+        songs={songs} 
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio 
+        onLoadedMetadata={timeUpdateHandler} 
+        onTimeUpdate={timeUpdateHandler} 
+        ref={audioRef} 
+        src={currentSong.audio}>
+      </audio>
     </div>
   );
 }
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,12 +1,38 @@
 import React from "react";
 
-const LibrarySong = ({ song, songs, setCurrentSong, id, key }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id, key, audioRef, isPlaying, setSongs }) => {
     const songSelectHandler = () => {
         const selectedSong = songs.filter((state) => state.id === id);
         setCurrentSong(selectedSong[0]);
+        //Agregamos state active o estado activo
+        const newSongs = songs.map((song) => {
+            if(song.id === id) {
+                return{
+                    ...song, 
+                    active: true,
+
+                }
+            }
+            else{
+                return{
+                    ...song,
+                    active: false,
+                }
+            }
+        });
+        setSongs(newSongs)
+        //Chequeamos si la cancion se esta reproduciendo
+        if(isPlaying){
+            const playPromise = audioRef.current.play();
+            if(playPromise !== undefined){
+                playPromise.then((audio) => {
+                    audioRef.current.play();
+                });
+            }
+        }
     }   
     return(
-        <div onClick={songSelectHandler} className="library-song">
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
             <img alt={song.name} src={song.cover} />
             <div className="song-description">
                 <h3>{song.name}</h3>
